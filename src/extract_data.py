2d91b7471a77d4087d71275b71808de755be0db4
Thiago Ferraz <thiagovdferraz@gmail.com>
2026-03-19 22:56:28 -0300

    extract_data atualizado

--- src/extract_data.py
+++ src/extract_data.py
@@ -1,32 +1,29 @@
-import requests
+import requests 
 import json
 from pathlib import Path
 
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+
 def extract_weather_data(url:str) -> list:
     response = requests.get(url)
     data = response.json()
-
+    
     if response.status_code != 200:
-        logging.error(f"Erro na requisição: {response.status_code} - {response.text}")
+        logging.error("Erro na requisição")
         return []
     
     if not data:
-        logging.warning("Nenhum dado encontrado.")
+        logging.warn("Nenhum dado retornado")
         return []
-
-
-    outupt_path = 'data/weather_data.json'
-    outupt_dir = Path(outupt_path).parent
-    outupt_dir.mkdir(parents=True, exist_ok=True)
-
-    with open(outupt_path, 'w') as f:
-        json.dump(data, f, indent=4) # Save the data to a JSON file
-
-    logging.info(f"Dados salvos em: {outupt_path}")
+    
+    output_path = 'data/weather_data.json'
+    output_dir = Path(output_path).parent
+    output_dir.mkdir(parents=True, exist_ok=True)
+    
+    with open(output_path, 'w') as f:
+        json.dump(data, f, indent=4)
+    
+    logging.info(f"Arquivo salvo em {output_path}")  
     return data
-
-
-extract_weather_data(url)
